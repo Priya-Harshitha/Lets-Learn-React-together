c7d0fb8826ba7329dcf6879fe8c7952446d2691e
Priya-Harshitha <121441190+Priya-Harshitha@users.noreply.github.com>
2025-07-26 14:06:06 +0530

    Add files via upload

---
+++ learn-react/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} don't copy my learnings😜</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ learn-react/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return (
+        <header>
+            <h1>Welcome to My-Learnings 😉</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">profile</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    )
+}
+
+export default Header
